c4ea94b26348c7f0223d1a72127b9fd84cc5a0cb
Fatih Yapıcı <fa.yapici@gmail.com>
2023-09-28 10:56:54 +0300

    migrations and db update

---
+++ Migrations/20230927122011_InitialCreate.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApartmentManagement.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Invoice",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Session = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Residence = table.Column<string>(type: "TEXT", nullable: true),
+                    Amount = table.Column<decimal>(type: "decimal(18, 2)", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Invoice", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Invoice");
+        }
+    }
+}
